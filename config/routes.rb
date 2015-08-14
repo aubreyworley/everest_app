5fca4b4735e16b44351c5d82837db5c0d315391f
aubreyworley <aubreyworley@gmail.com>
2015-08-14 13:20:00 -0700

    app setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,28 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :users, except: [:destroy]
+  resources :maps
+  root "pages#home" 
 end
+
+#       Prefix Verb   URI Pattern               Controller#Action
+#    users_new GET    /users/new(.:format)      users#new
+# users_create GET    /users/create(.:format)   users#create
+#   users_show GET    /users/show(.:format)     users#show
+#   users_edit GET    /users/edit(.:format)     users#edit
+# users_update GET    /users/update(.:format)   users#update
+#   pages_home GET    /pages/home(.:format)     pages#home
+#        users GET    /users(.:format)          users#index
+#              POST   /users(.:format)          users#create
+#     new_user GET    /users/new(.:format)      users#new
+#    edit_user GET    /users/:id/edit(.:format) users#edit
+#         user GET    /users/:id(.:format)      users#show
+#              PATCH  /users/:id(.:format)      users#update
+#              PUT    /users/:id(.:format)      users#update
+#         maps GET    /maps(.:format)           maps#index
+#              POST   /maps(.:format)           maps#create
+#      new_map GET    /maps/new(.:format)       maps#new
+#     edit_map GET    /maps/:id/edit(.:format)  maps#edit
+#          map GET    /maps/:id(.:format)       maps#show
+#              PATCH  /maps/:id(.:format)       maps#update
+#              PUT    /maps/:id(.:format)       maps#update
+#              DELETE /maps/:id(.:format)       maps#destroy
